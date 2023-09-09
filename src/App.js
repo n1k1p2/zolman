046d1a5af2b7e07adf7e425662c46fe43c0542ba
n1k1p2 <nikpyatin2@gmail.com>
2023-09-10 01:30:51 +0300

    fadsdsfsdf

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import zolmanlogo from './zolmanio.jpg';
+import zolmanmusic from './zol.mp4';
+import { Player } from 'video-react';
+
 function App() {
   return (
     <div className="App">
+      <video autoPlay>
+        <source src={zolmanmusic} />
+        </video>
+      
       <h1>ВНИМАНИЕ!!!</h1>
+
       <h1>РАЗЫСКИВАЕТСЯ ПЕДОФИЛ, МАНЬЯК, УБИЙЦА, ТЕРРОРИСТ</h1>
       <img className='manyak-pedofil' src={zolmanlogo}></img>
       <div className='text-desc'>
